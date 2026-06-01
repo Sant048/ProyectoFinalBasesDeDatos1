0fb30b28380e5642cab025b97dcae261595b5ffd
Your Name <you@example.com>
2026-05-31 22:16:47 -0500

    Proyecto spring boot Base

---
+++ proyecto-bases1/src/main/java/com/Bases1/proyecto_bases1/ProyectoBases1Application.java
@@ -0,0 +1,13 @@
+package com.Bases1.proyecto_bases1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBases1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBases1Application.class, args);
+	}
+
+}
---
+++ proyecto-bases1/src/test/java/com/Bases1/proyecto_bases1/ProyectoBases1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bases1.proyecto_bases1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBases1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
